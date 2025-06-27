b186cff712472514e3ad8cfe3877b3ec494e7513
Simon Wilson <sjw@paratusltd.com>
2025-06-27 08:54:42 +0100

    hash router for hosting

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { App } from "./app";
 import { Auth0ProviderWithNavigate } from "./auth0-provider-with-navigate";
 import "./styles/styles.css";
@@ -11,10 +11,10 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Auth0ProviderWithNavigate>
         <App />
       </Auth0ProviderWithNavigate>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
